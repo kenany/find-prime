58116e558e803ab72eb21a7b46a4443a217d39bd
Kenan Yildirim <kenan@kenany.me>
2016-09-14 14:03:57 -0400

    test: use `t.error`
    Closes https://github.com/KenanY/find-prime/issues/14

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 var findPrime = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
-var isNull = require('lodash.isnull');
 var BigInteger = require('bigi');
 
 test('exports a function', function(t) {
@@ -13,7 +12,7 @@ test('returns a big integer', function(t) {
   t.plan(3);
   t.doesNotThrow(function() {
     findPrime(100, function(error, prime) {
-      t.ok(isNull(error));
+      t.error(error);
       t.ok(prime instanceof BigInteger);
     });
   });
